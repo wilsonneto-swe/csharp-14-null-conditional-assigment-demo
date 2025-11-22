675d14f8848c0536b2374e9933a912ab6cee8577
Wilson Gomes <20674439+wilsongomes-swe@users.noreply.github.com>
2025-11-22 20:35:17 -0300

    add example code

---
+++ NullConditionalAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿Dictionary<string, string> dict = null; // new();
+
+/* C# 13 way
+if (dict is null)
+{
+   dict["chave"] = "valor";
+}
+*/
+
+// C# 14
+dict?["chave"] = "valor";
+
+
+User? user = new User("Name", "Second Name");
+
+/* C# 13 way
+if (user is not null)
+{
+    user.Name = "Test";
+}
+*/
+
+// C# 14
+user?.Name = "Test";
+
+Console.WriteLine(user?.Name ?? "No user...");
+
+internal class User(string name, string lastName)
+{
+    public string Name { get; set; } = name;
+    public string LastName { get; set; } = lastName;
+}
